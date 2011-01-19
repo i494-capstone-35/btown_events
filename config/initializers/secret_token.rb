437801f9d15875765c5a49833a3d62e64bc6f916
Matt Sacks <matt.s.sacks@gmail.com>
2011-01-19 08:08:40 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BtownEvents::Application.config.secret_token = 'ae689a11edf7d904ea31c1c059c52194bf3a504e78b2c98a17b281a0fced6123543a44cdc6160a6762977caf6cee5c4fce98cc66a8c9f038940560b41ca026b9'
