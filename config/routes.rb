ea9ca52dcc0fae32a8e4868b969e651e44e3d784
Matt Sacks <matt.s.sacks@gmail.com>
2011-02-01 19:33:15 -0500

    Add Facility model and files

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 BtownEvents::Application.routes.draw do
+  resources :facilities
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
---
+++ db/migrate/20110130163608_create_facilities.rb
@@ -0,0 +1,16 @@
+class CreateFacilities < ActiveRecord::Migration
+  def self.up
+    create_table :facilities do |t|
+      #t.has_many :events
+      t.string :name
+      t.string :number
+      t.string :address
+      t.string :website
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :facilities
+  end
+end
