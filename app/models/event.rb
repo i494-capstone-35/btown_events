2f294faebcec9a375363d5ea79cc775ff8422c5d
Matt Sacks <matt.s.sacks@gmail.com>
2011-02-12 12:42:30 -0500

    Add more CSS and HTML to events/the app

--- app/models/event.rb
+++ app/models/event.rb
@@ -8,9 +8,5 @@ class Event < ActiveRecord::Base
   end
   MONTHS = [31,feb,31,30,31,30,31,31,30,31,30,31]
 
-  current_month = Integer Time.now.strftime("%m")
-  days = MONTHS[current_month]
-  #m = Integer Time.now.strftime("%m")
-
   named_scope :months_events, lambda { |time| where(:date => (time.beginning_of_month..time.end_of_month)) }
 end
