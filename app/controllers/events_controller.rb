7457400d762d7562031fb5e612410e89916e1f41
Matt Sacks <matt.s.sacks@gmail.com>
2011-03-06 18:40:33 -0500

    Add monthly view for Events but doesn't increment properly.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,4 @@
 class EventsController < ApplicationController
-
   def increment
     n = params[:weeks].to_i
     @marker = Date.today.beginning_of_week + n.weeks
@@ -19,9 +18,18 @@ class EventsController < ApplicationController
   def show
     @event = Event.find(params[:id])
 
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @event }
-    end
+    respond_to :html
+  end
+
+  def month
+    @events = Event.all
+    @count = Event.count
+    @marker = Date.today.beginning_of_month.beginning_of_week
+    @weeks = (Time.days_in_month Date.current.month) / 7
+    @extras = (Time.days_in_month Date.current.month) % 7
+    @days = 7
+    @week_events = Event.weeks_events @marker
+
+    respond_to :html
   end
 end
