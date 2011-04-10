f47e6c32c437b372738f57c17069489b5e5afe69
Matt Sacks <matt.s.sacks@gmail.com>
2011-04-10 11:40:46 -0400

    More mobile user strings

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 BtownEvents::Application.routes.draw do
+
   # mobile
-  root :to => 'events#date', :constraints => {:user_agent => /Mobile|webOS|BlackBerry/}
+  MOBILE_USER_AGENTS = 'palm|blackberry|nokia|phone|midp|mobi|symbian|chtml|ericsson|minimo|' +
+    'audiovox|motorola|samsung|telit|upg1|windows ce|ucweb|astel|plucker|' +
+    'x320|x240|j2me|sgh|portable|sprint|docomo|kddi|softbank|android|mmp|' +
+    'pdxgw|netfront|xiino|vodafone|portalmmm|sagem|mot-|sie-|ipod|up\\.b|' +
+    'webos|amoi|novarra|cdm|alcatel|pocket|ipad|iphone|mobileexplorer|' +
+    'mobile'
+
+  root :to => 'events#date', :constraints => {:user_agent => Regexp.new(MOBILE_USER_AGENTS, true)}
   match '/date/:year/:month/:day' => 'events#date', 
-    :constraints => {:user_agent => /Mobile|webOS|BlackBerry/}
+    :constraints => {:user_agent => Regexp.new(MOBILE_USER_AGENTS, true)}
 
   # web
   root :to => 'events#index'
